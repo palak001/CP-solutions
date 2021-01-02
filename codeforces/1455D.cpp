950a82fb84b8e8be5059746c53c435fbfc4336fe
Palak <palak7372@gmail.com>
2021-01-02 19:18:40 +0530

    Create 1455D.cpp

---
+++ codeforces/1455D.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--) {
+		int n, x;
+		cin >> n >> x;
+		vector<int>a(n);
+		int check, flag = 0;
+		for(int i = 0; i < n; i++) {
+			cin >> a[i];
+			if(i == 0) {
+				check = a[i];
+			}
+			else {
+				if(a[i] < check)
+					flag = 1;
+				check = a[i];
+			}
+		}
+		
+		if(flag == 0) {
+			cout << 0 << endl;
+			continue;
+		}
+		
+		int ind;
+		check = a[0];
+		flag = 0;
+		for(ind = 0; a[ind] < x && ind < n; ind++) {
+			if(check > a[ind]) {
+				flag = 1;
+				break;
+			}
+			check = a[ind];
+		}
+		if(ind == n) {
+			cout << -1 << endl;
+			continue;
+		}
+		int ans = 0, val, ans2 = 0;
+		if(ind == 0) val = INT_MIN;
+		else val = a[ind-1];
+		flag = 0;
+		for(; ind < n; ind++) {
+			if(a[ind] < val) {
+				flag = 1;
+				break;
+			}
+			else if(a[ind] > x){
+				val = x;
+				x = a[ind];
+				ans++;
+			}
+			else if(a[ind] == x) {
+				val = x;
+			}
+			else {
+				ans2 += ans;
+				val = a[ind];
+				ans = 0;
+			}
+		}
+		if(flag == 1)
+			cout << -1 << endl;
+		else {
+			// if(ans2 == -1)
+			// 	ans2 = ans;
+			cout << ans2 << endl;
+		}
+	}
+}
